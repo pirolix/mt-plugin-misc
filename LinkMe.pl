a3359f5ac15ec774b30935e718a0b3ff52a2a6c4
piroli <piroli@adbff654-c4ea-448a-8ae3-cad037306009>
2010-02-06 08:12:00 +0000

    LinkMe 0.01
    git-svn-id: svn://svn.magicvox.net/var/svn/mt-plugins/__misc@128 adbff654-c4ea-448a-8ae3-cad037306009

---
+++ LinkMe.pl
@@ -0,0 +1,25 @@
+package MT::Plugin::OMV::LinkMe;
+
+use strict;
+use MT 4;
+use MT::Template::Context;
+
+use vars qw( $MYNAME $VERSION );
+$MYNAME = 'LinkMe';
+$VERSION = '0.01';
+
+### BuildPage callback
+MT->add_callback ('BuildFileFilter', 9, undef, sub {
+    my (undef, %args) = @_;
+    $args{Context}->stash ($MYNAME. '::file_info', $args{FileInfo});
+    1;
+});
+
+### Functional tag
+MT::Template::Context->add_tag ($MYNAME => sub {
+    my $finfo = $_[0]->stash ($MYNAME. '::file_info')
+        or return '';
+    $finfo->url || '';
+});
+
+1;
