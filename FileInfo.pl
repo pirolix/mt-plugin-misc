9ea5d87202c4dfbdb810a7189447c78451273ec8
piroli <piroli@adbff654-c4ea-448a-8ae3-cad037306009>
2010-02-09 17:22:56 +0000

    FileInfo 0.01
    git-svn-id: svn://svn.magicvox.net/var/svn/mt-plugins/__misc@132 adbff654-c4ea-448a-8ae3-cad037306009

---
+++ FileInfo.pl
@@ -0,0 +1,28 @@
+package MT::Plugin::OMV::FileInfo;
+
+use strict;
+use MT 4;
+use MT::Template::Context;
+
+use vars qw( $MYNAME $VERSION );
+$MYNAME = 'FileInfo';
+$VERSION = '0.01';
+
+### BuildPage callback
+MT->add_callback ('BuildFileFilter', 9, undef, sub {
+    my (undef, %args) = @_;
+    $args{Context}->stash ($MYNAME. '::file_info', $args{FileInfo});
+    1;
+});
+
+### Functional tag
+MT::Template::Context->add_tag ($MYNAME => sub {
+    my ($finfo, $column);
+
+    ($finfo = $_[0]->stash ($MYNAME. '::file_info'))
+        && ($column = $_[1]->{column} || 'url')
+        && $finfo->can ($column)
+        && $finfo->$column || '';
+});
+
+1;
