6bac8ec7eb4aa0054f53ad678ca140b5067d33bb
piroli <piroli@adbff654-c4ea-448a-8ae3-cad037306009>
2011-01-26 14:08:30 +0000

    0.00_01
    git-svn-id: svn://svn.magicvox.net/var/svn/mt-plugins/__misc@187 adbff654-c4ea-448a-8ae3-cad037306009

---
+++ JapaneseYear.pl
@@ -0,0 +1,52 @@
+package MT::Plugin::OMV::JapaneseYear;
+
+use strict;
+use MT::Template::Context;
+
+use vars qw( $MYNAME $VERSION );
+$MYNAME = (split /::/, __PACKAGE__)[-1];
+$VERSION = '0.00_01';
+
+use base qw( MT::Plugin );
+my $plugin = __PACKAGE__->new({
+        id => $MYNAME,
+        key => $MYNAME,
+        name => $MYNAME,
+        version => $VERSION,
+        author_name => 'Open MagicVox.net',
+        author_link => 'http://www.magicvox.net/',
+        doc_link => 'http://www.kumamotokokufu-h.ed.jp/kumamoto/bungaku/wa_seireki.html',
+        description => <<HTMLHEREDOC,
+<__trans phrase="Convert the Christian Era to the Japanese one.">
+HTMLHEREDOC
+});
+MT->add_plugin( $plugin );
+
+### Modifier - japanese_year
+MT::Template::Context->add_global_filter (japanese_year => sub {
+    my ($date, $args) = @_;
+
+    my %date_table = (
+#       '元号' => [ YYYY_MM_DD, YYYY_MM_DD ],
+        '明治' => [ 1868_09_08, 1912_07_29 ],
+        '大正' => [ 1912_07_30, 1926_12_24 ],
+        '昭和' => [ 1926_12_25, 1989_01_07 ],
+        '平成' => [ 1989_01_08, 2999_12_31 ],
+    );
+
+    $date =~ s!(\d{8})!sub {
+        my ($datetime) = @_;
+        while (my ($name, $table) = each %date_table) {
+            if ($table->[0] <= $datetime && $datetime <= $table->[1]) {
+                $args = sprintf '%s%d',
+                    $name, int ($datetime / 1_00_00) - int ($table->[0] / 1_00_00) + 1;
+                $args = Encode::decode ('utf-8', $args) if 5.0 <= $MT::VERSION;;
+                last;
+            }
+        }
+        $args;
+    }->($1)!e;
+    $date;
+});
+
+1;
