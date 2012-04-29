76b952d45795648bf251e01a1a9dcaab5fcf6604
piroli <piroli@adbff654-c4ea-448a-8ae3-cad037306009>
2012-04-29 08:12:59 +0000

    * こっそり追加
    git-svn-id: svn://svn.magicvox.net/var/svn/mt-plugins/__misc@227 adbff654-c4ea-448a-8ae3-cad037306009

---
+++ NoBasename.pl
@@ -0,0 +1,59 @@
+package MT::Plugin::Editing::OMV::NoBasename;
+# $Id$
+
+use strict;
+use MT 5;
+
+use vars qw( $VENDOR $MYNAME $VERSION );
+($VENDOR, $MYNAME) = (split /::/, __PACKAGE__)[-2, -1];
+(my $revision = '$Rev$') =~ s/\D//g;
+$VERSION = '0.10'. ($revision ? ".$revision" : '');
+
+use base qw( MT::Plugin );
+my $plugin = __PACKAGE__->new ({
+    id => $MYNAME,
+    key => $MYNAME,
+    name => $MYNAME,
+    version => $VERSION,
+    author_name => 'Open MagicVox.net',
+    author_link => 'http://www.magicvox.net/',
+    doc_link => 'http://lab.magicvox.net/trac/mt-plugins/wiki/NoBasename', # trac
+    description => <<'HTMLHEREDOC',
+<__trans phrase="Hide and fill with the unique value into the basename field.">
+HTMLHEREDOC
+    registry => {
+        callbacks => {
+            'MT::App::CMS::template_source.edit_entry' => \&_hdlr_source_edit_entry,
+        },
+    },
+});
+MT->add_plugin ($plugin);
+
+
+
+###
+sub _hdlr_source_edit_entry {
+    my ($cb, $app, $tmpl) = @_;
+
+    my $old = quotemeta (<<'MTMLHEREDOC');
+<mt:include name="include/footer.tmpl" id="footer_include">
+MTMLHEREDOC
+
+    my $new = <<'MTMLHEREDOC';
+<mt:setvarblock name="jq_js_include" append="1">
+<mt:if name="object_type" eq="entry">
+    // 
+    jQuery('#basename-field').hide();
+<mt:unless name="id">
+    // 
+    jQuery('#entry_form').submit(function () {
+        this.basename.value = (new Date()).getTime();
+        return true;
+    });
+</mt:unless></mt:if>
+</mt:setvarblock>
+MTMLHEREDOC
+    $$tmpl =~ s/($old)/$new$1/;
+}
+
+1;
---
+++ NoToolbar.pl
@@ -0,0 +1,59 @@
+package MT::Plugin::Editing::OMV::NoToolbar;
+# $Id$
+
+use strict;
+use MT 5;
+
+use vars qw( $VENDOR $MYNAME $VERSION );
+($VENDOR, $MYNAME) = (split /::/, __PACKAGE__)[-2, -1];
+(my $revision = '$Rev$') =~ s/\D//g;
+$VERSION = '0.10'. ($revision ? ".$revision" : '');
+
+
+use base qw( MT::Plugin );
+my $plugin = __PACKAGE__->new ({
+    id => $MYNAME,
+    key => $MYNAME,
+    name => $MYNAME,
+    version => $VERSION,
+    author_name => 'Open MagicVox.net',
+    author_link => 'http://www.magicvox.net/',
+    doc_link => 'http://lab.magicvox.net/trac/mt-plugins/wiki/NoToolbar', # trac
+    description => <<'HTMLHEREDOC',
+<__trans phrase="Hide the default toolbars of the textarea">
+HTMLHEREDOC
+    registry => {
+        callbacks => {
+            'MT::App::CMS::template_source.edit_entry' => \&_hdlr_template_source_archetype_editor,
+        },
+    },
+});
+MT->add_plugin ($plugin);
+
+
+
+###
+sub _hdlr_template_source_archetype_editor {
+    my ($cb, $app, $tmpl) = @_;
+
+    my $old = quotemeta (<<'MTMLHEREDOC');
+<mt:include name="include/header.tmpl" id="header_include">
+MTMLHEREDOC
+
+    my $new = <<'MTMLHEREDOC';
+<mt:setvarblock name="html_head" append="1">
+<style type="text/css">
+#editor-content-toolbar {
+    padding: 0px;
+    border-bottom: none;
+}
+#editor-content-toolbar div.field-buttons-formatting {
+    display: none;
+}
+</style>
+</mt:setvarblock>
+MTMLHEREDOC
+    $$tmpl =~ s/($old)/$new$1/;
+}
+
+1;
