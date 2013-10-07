d640edc6672f946e2ed4d88caed24c71e610ef71
pirolix <pirolix@adbff654-c4ea-448a-8ae3-cad037306009>
2013-10-07 05:32:49 +0000

    * プチ改造  * ヘッダ更新
    git-svn-id: svn://svn.magicvox.net/var/svn/mt-plugins/__misc@375 adbff654-c4ea-448a-8ae3-cad037306009

--- Anchors.pl
+++ Anchors.pl
@@ -1,22 +1,29 @@
-package MT::Plugin::OMV::Anchors;
+package MT::Plugin::Filter::OMV::Anchors;
+# Anchors (C) 2013 Piroli YUKARINOMIYA (Open MagicVox.net)
+# This program is distributed under the terms of the GNU Lesser General Public License, version 3.
+# $Id: Anchors.pl 344 2013-07-20 07:49:34Z pirolix $
 
 use strict;
+use warnings;
 use MT 3;
 
-use vars qw( $MYNAME $VERSION );
-$MYNAME = (split /::/, __PACKAGE__)[-1];
-$VERSION = '0.00_01';
+use vars qw( $VENDOR $MYNAME $FULLNAME $VERSION );
+$FULLNAME = join '::',
+        (($VENDOR, $MYNAME) = (split /::/, __PACKAGE__)[-2, -1]);
+(my $revision = '$Rev: 344 $') =~ s/\D//g;
+$VERSION = 'v0.10'. ($revision ? ".$revision" : '');
 
 use base qw( MT::Plugin );
 my $plugin = __PACKAGE__->new ({
-        id => $MYNAME,
-        key => $MYNAME,
-        name => $MYNAME,
-        version => $VERSION,
-        author_name => 'Open MagicVox.net',
-        author_link => 'http://www.magicvox.net/',
-        doc_link => 'http://www.magicvox.net/archive/2011/02131853/',
-        description => <<HTMLHEREDOC,
+    id => $FULLNAME,
+    key => $FULLNAME,
+    name => $MYNAME,
+    version => $VERSION,
+    author_name => 'Open MagicVox.net',
+    author_link => 'http://www.magicvox.net/',
+    plugin_link => 'http://www.magicvox.net/archive/2011/02131853/', # Blog
+    doc_link => "http://lab.magicvox.net/trac/mt-plugins/wiki/$MYNAME", # tracWiki
+    description => <<HTMLHEREDOC,
 <__trans phrase="Easy way to refer and arrange the items in the article.">
 HTMLHEREDOC
 });
@@ -37,14 +44,14 @@ sub _filter_anchors {
     $text =~ s!<(?:$tag)\s+name="(.+?)"\s*/?>!sub {
         my ($name) = @_;
         $items{$name} = ++$num;
-        sprintf qq{<a name="$anchor">$format</a>}, $tag,$num, $num;
+        sprintf qq{<a name="$anchor">$format</a>}, $tag,$num, $num, $name;
     }->($1)!ige;
 
     # Replace with link
     $text =~ s!<(?:$tag)\s+href="#?(.+?)"\s*/?>!sub {
         my ($name) = @_;
         my $num = $items{$name} || 0;
-        sprintf qq{<a href="#$anchor">$format</a>}, $tag,$num, $num;
+        sprintf qq{<a href="#$anchor">$format</a>}, $tag,$num, $num, $name;
     }->($1)!ige;
 
     $text;
